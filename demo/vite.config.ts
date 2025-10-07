54a3a3ac9e7fcd18f7194dbf77f6b47fd33d60b4
Caio Pizzol <caio@harbourshare.com>
2025-10-07 17:30:30 -0300

    refactor: remove optimization dependency exclusion for superdoc

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,9 +5,6 @@ import path from 'path';
 export default defineConfig({
     plugins: [react()],
     base: '/',
-    optimizeDeps: {
-        exclude: ['@superdoc-dev/esign']
-    },
     resolve: {
         alias: {
             'superdoc/dist/style.css': path.resolve(__dirname, '../node_modules/superdoc/dist/style.css')
